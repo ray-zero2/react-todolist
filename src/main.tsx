e155ac0c22f8123bacd0b35e9dd44c76df1aae9c
Rei Matsuda <rei2219.matsuda@gmail.com>
2021-09-23 03:11:16 +0900

    add reset components

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
 import './index.css'
 import App from './App'
 
+const SGlobalStyle = createGlobalStyle`
+  ${reset}
+`
+
 ReactDOM.render(
   <React.StrictMode>
+    <SGlobalStyle />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
